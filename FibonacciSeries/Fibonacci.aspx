0d83cb55b4bf96651926cb5be6774c1678cf92c0
Zoya Ansari <ansari.zoyaa13@gmail.com>
2024-08-02 01:37:48 +0530

    Fibonacci Series GUI

---
+++ FibonacciSeries/Fibonacci.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Fibonacci.aspx.cs" Inherits="FibonacciSeries.Fibonacci" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label runat="server" Text="Fibonacci Series"></asp:Label>
+    
+    </div>
+    <p>
+        <asp:Label ID="Label2" runat="server" Text="Enter The Limit"></asp:Label>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" onclick="Fibonacci_Series" Text="Result" />
+    </p>
+    <p>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </p>
+    </form>
+</body>
+</html>
---
+++ FibonacciSeries/Fibonacci.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FibonacciSeries
+{
+    public partial class Fibonacci : System.Web.UI.Page
+    {
+        private int a, b,input, sum;
+
+        public String fibonacci()
+        {
+            a = 0;
+            b = 1;
+            string s = System.Convert.ToString(a+  "\t" +b);
+            for (int i = 2; i < input; i++)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+                s = System.Convert.ToString(s + "\t" +sum);
+               
+            }
+            return s;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void Fibonacci_Series(object sender, EventArgs e)
+        {
+            Fibonacci obj = new Fibonacci();
+            obj.input = System.Convert.ToInt32(TextBox1.Text);
+            TextBox2.Text = obj.fibonacci(); 
+
+    
+        }
+    }
+}
